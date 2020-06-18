bf4f41ffea282b51b3d639aa0fda5d069120bd89
Sarah <s.schauppenlehner@gmx.at>
2020-06-18 17:24:52 +0200

    Add navigation component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
+    AppComponent,
     HeaderComponent,
     NavigationComponent
   ],
