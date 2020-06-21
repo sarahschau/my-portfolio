ee76211fcf3638ca61da6074d7841783627446b0
Sarah <s.schauppenlehner@gmx.at>
2020-06-21 16:43:25 +0200

    Update projects.

--- src/app/mock-projects.ts
+++ src/app/mock-projects.ts
@@ -2,57 +2,75 @@ import { Project } from './project';
 
 export const PROJECTS: Project[] = [
     {
-        title: 'Account your future',
-        link: 'https://accountyourfuture.nl',
-        description: 'Projekt im Zuge meines Praktikums in Amsterdam',
-        technologies: ['JavaScript', 'Vue.js', 'Laravel', 'Stylus', 'Molly API']
-    },
-    {
         title: 'MAV',
         link: 'https://mav.at',
-        description: 'Website für den Österreichischen Markenartikel Verbankd',
-        technologies: ['JavaScript', 'Nuxt.js', 'Wordpress', 'SCSS']
+        description: 'Website for the Österreichischen Markenartikel Verband.',
+        technologies: ['JavaScript', 'Nuxt.js', 'Wordpress', 'SCSS'],
+        coverImage: 'mav.png',
+        backgroundColor: '#497C62'
     },
     {
         title: 'FEEI',
-        link: 'https://feei.at',
-        description: 'Website für den Fachverband der Elekto- und Elektrotechnik',
-        technologies: ['Wordpress', 'SCSS']
+        link: 'https://www.feei.at',
+        description: 'Website for the Fachverband der Elekto- und Elektrotechnik.',
+        technologies: ['Wordpress', 'SCSS'],
+        coverImage: 'feei-1.png',
+        backgroundColor: '#3E4E6D'
     },
     {
         title: 'H2i Green Hydrogen',
         link: 'https://greenhydrogen.at/',
-        description: 'Website für die H2i Green Hydrogen GmbH (H2i)',
-        technologies: ['Nuxt.js', 'Strapi' , 'SCSS']
+        description: 'Website for the H2i Green Hydrogen GmbH (H2i).',
+        technologies: ['Nuxt.js', 'Strapi' , 'SCSS'],
+        coverImage: 'h2i.png',
+        backgroundColor: '#FFFFFF'
     },
     {
         title: 'Vöslauer Lauf los',
         link: 'https://lauflos.voeslauer.com/',
-        description: 'Microsite eine Running Kampagne von Vöslauer',
-        technologies: ['Nuxt.js', 'Strapi' , 'SCSS']
+        description: 'Microsite for a campaign with Vöslauer.',
+        technologies: ['Nuxt.js', 'Strapi' , 'SCSS'],
+        coverImage: 'voeslauer-running.png',
+        backgroundColor: '#4A8275'
     },
     {
         title: 'Darbo Fruchtikurs',
         link: 'https://fruchtikus.darbo.at/',
-        description: 'Interaktive Rezepte Seite für Darbo',
-        technologies: ['Nuxt.js', 'GSAP', 'Scrollmagic', 'SCSS']
+        description: 'Interactive Recipe Website for Darbo.',
+        technologies: ['Nuxt.js', 'GSAP', 'Scrollmagic', 'SCSS'],
+        coverImage: 'fruchtikurs.png',
+        backgroundColor: '#F0EFE9'
     },
     {
-        title: 'dynamische Banner für bob',
-        link: '',
-        description: '',
-        technologies: ['Google Web Designer']
+        title: 'OIS OK',
+        link: 'https://lebedasleben.com/',
+        description: 'Campagin Website for Wiener Städtische.',
+        technologies: ['Nuxt.js', 'HTML', 'SCSS'],
+        coverImage: 'ois-ok.png',
+        backgroundColor: '#B40317'
     },
     {
-        title: 'Marken-Emojis App',
-        link: '',
-        description: 'Eine native App im Zuge der Achten Sie auf die Marke Kampagne zum Hinzufügen von Sticker Sets',
-        technologies: ['Flutter', 'Dart']
+        title: 'Account your future',
+        link: 'https://www.accountyourfuture.com',
+        description: 'Project during my Internship in Amsterdam for Account your Future.',
+        technologies: ['JavaScript', 'Vue.js', 'Laravel', 'Stylus', 'Molly API'],
+        coverImage: 'account.png',
+        backgroundColor: '#2C2C2C'
     },
-    {
-        title: 'Vöslauer AR',
-        link: '',
-        description: 'Eine Website mit ',
-        technologies: ['NuxtJs', 'SCSS']
-    }
+    // {
+    //     title: 'Marken-Emojis App',
+    //     link: '',
+    //     description: 'Eine native App im Zuge der Achten Sie auf die Marke Kampagne zum Hinzufügen von Sticker Sets',
+    //     technologies: ['Flutter', 'Dart'],
+    //     coverImage: '',
+    //     backgroundColor: '#3E4E6D'
+    // },
+    // {
+    //     title: 'Vöslauer AR',
+    //     link: '',
+    //     description: 'Eine Website mit ',
+    //     technologies: ['NuxtJs', 'SCSS'],
+    //     coverImage: '',
+    //     backgroundColor: '#3E4E6D'
+    // }
 ];
--- src/app/project.ts
+++ src/app/project.ts
@@ -1,6 +1,10 @@
+type Nullable<T> = T | null;
+
 export interface Project {
     title: string;
     link: string;
     description: string;
     technologies: Array<string>;
+    coverImage: Nullable<string>;
+    backgroundColor: string;
 }
